c9db7b42b350308280d17a6728c123d233408e8a
lihaitao <lihaitao_jkcpx@kedacom.com>
2020-04-27 20:21:16 +0800

    maven整理

--- boot-cache/src/test/java/org/lht/boot/cache/redis/RedisTest.java
+++ boot-cache/src/test/java/org/lht/boot/cache/redis/RedisTest.java
@@ -1,8 +1,13 @@
 package org.lht.boot.cache.redis;
 
+import com.google.common.collect.Lists;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author LiHaitao
@@ -10,6 +15,7 @@ import org.springframework.boot.test.context.SpringBootTest;
  * @date 2020/4/26 9:59
  **/
 @SpringBootTest
+@Slf4j
 public class RedisTest {
 
     @Autowired
@@ -19,5 +25,14 @@ public class RedisTest {
     public void test01() {
 
         redisUtil.get("sdf");
+    }
+
+    @Test
+    public void test02() {
+        ArrayList<String> strings = Lists.newArrayList("01", "02", "03");
+        redisUtil.lSet("key", strings);
+
+        List list = redisUtil.lGet("key", 0, -1);
+        log.info("list:{}", list);
     }
 }
