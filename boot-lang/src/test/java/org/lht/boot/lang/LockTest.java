b69f01ce50bcc7959adf3048d4d3c7007b2f7340
lihaitao <984695425@qq.com>
2020-05-25 22:27:12 +0800

    重入锁的test代码

--- boot-lang/src/test/java/org/lht/boot/lang/LockTest.java
+++ boot-lang/src/test/java/org/lht/boot/lang/LockTest.java
@@ -1,8 +1,12 @@
 package org.lht.boot.lang;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
@@ -11,20 +15,57 @@ import java.util.concurrent.locks.ReentrantLock;
  * @Author lht
  * @Date 2020/3/11 7:27 PM
  **/
+@Slf4j
 public class LockTest {
-    public static int j=0;
+    public static int j = 0;
+    Lock lock;
+
+    @Before
+    public void before() {
+        lock = new ReentrantLock();
+
+    }
 
     @Test
     public void test01() throws InterruptedException {
-//        final ReentrantLock lock=new ReentrantLock();
-//        for (int i=0;i<1;i++){
-//            lock.lock();
-//            try{
-//                j++;
-//            }finally {
-//                lock.unlock();
-//            }
-//        }
-//        Assert.assertEquals(j,100000000);
+        for (int i = 0; i < 100000000; i++) {
+            lock.lock();
+            try {
+                j++;
+            } finally {
+                lock.unlock();
+            }
+        }
+        Assert.assertEquals(j, 100000000);
+    }
+
+    @Test
+    public void test02() throws InterruptedException {
+        new Thread(()->{
+            lock.lock();
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }finally {
+                lock.unlock();
+            }
+
+
+        },"线程-A").start();
+        new Thread(()->{
+            try {
+                if (lock.tryLock(2,TimeUnit.SECONDS)){
+                    log.info("获取到了锁");
+                }
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+
+        },"线程-B").start();
+        Thread.sleep(5000);
+        System.out.println(lock.tryLock());
     }
 }
