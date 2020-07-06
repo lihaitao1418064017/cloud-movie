b94e5e7659fe0b0001c2407e91c4856933601aa6
lihaitao <lihaitao_jkcpx@kedacom.com>
2020-07-06 18:22:41 +0800

    更新server的me接口

--- boot-security/security-oauth2/oauth2-server/src/main/java/org/lht/boot/security/server/controller/OAuth2AuthorityController.java
+++ boot-security/security-oauth2/oauth2-server/src/main/java/org/lht/boot/security/server/controller/OAuth2AuthorityController.java
@@ -1,11 +1,20 @@
 package org.lht.boot.security.server.controller;
 
 import org.lht.boot.security.core.common.util.OAuth2AuthenticationUtil;
+import org.lht.boot.security.core.vo.AuthenticationVO;
+import org.lht.boot.security.resource.entity.Permission;
+import org.lht.boot.security.resource.entity.UserInfo;
+import org.lht.boot.security.resource.service.PermissionService;
+import org.lht.boot.security.resource.service.RoleService;
+import org.lht.boot.security.resource.service.UserInfoService;
 import org.lht.boot.web.api.param.R;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author LiHaitao
@@ -16,11 +25,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class OAuth2AuthorityController {
 
+    @Autowired
+    private RoleService roleService;
+
+    @Autowired
+    private UserInfoService userInfoService;
+
+    @Autowired
+    private PermissionService permissionService;
 
     @GetMapping("/me")
     public R me() {
         Authentication authentication = OAuth2AuthenticationUtil.getAuthentication();
         String username = (String) authentication.getPrincipal();
+        UserInfo userInfo = userInfoService.selectByUsername(username);
+        AuthenticationVO authenticationVO = new AuthenticationVO();
+
+        List<Permission> permissions = permissionService.select(userInfo.getId());//todo 这里需要做成资源树
+        //                authenticationVO.setResources();
+
         return R.ok();
     }
 
