8a9a1af63ff7f9be0007c2f1bf365db357242eb0
lihaitao <984695425@qq.com>
2020-09-13 21:23:21 +0800

    添加注释

--- boot-security/security-oauth2/oauth2-core/src/main/java/org/lht/boot/security/core/param/UserQueryParam.java
+++ boot-security/security-oauth2/oauth2-core/src/main/java/org/lht/boot/security/core/param/UserQueryParam.java
@@ -5,7 +5,7 @@ import org.lht.boot.web.api.param.QueryParam;
 
 /**
  * @author LiHaitao
- * @description UserQueryParam:
+ * @description UserQueryParam:用户权限查询param
  * @date 2020/7/3 11:28
  **/
 @Data
